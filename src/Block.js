ed7d18cf675f3616f522497ffc23a5413d7fcc70
Chris Bacon <chrisbacon2009@hotmail.com>
2021-02-13 19:55:55 +0000

    Add mining function

--- src/Block.js
+++ src/Block.js
@@ -7,10 +7,19 @@ class Block {
     this.index = index;
     this.timestamp = timestamp;
     this.hash = this.createHash();
+    this.nonce = 0;
   }
 
   createHash() {
-    return SHA256(JSON.stringify(this.data) + this.previousHash + this.index + this.timestamp).toString();
+    return SHA256(JSON.stringify(this.data) + this.previousHash + this.index + this.timestamp + this.nonce).toString();
+  }
+
+  mine(difficulty) {
+    while (this.hash.substring(0, difficulty) !== Array(difficulty + 1).join("0")) {
+      this.nonce++;
+      this.hash = this.createHash();
+    }
+    console.log("block mined", this.hash);
   }
 }
 
